ea978e53d2ae0d1a066635629801ab8783ffae31
PhilSchoe <p.schoemacker@gmx.de>
2025-05-01 10:50:04 +0200

    Test nest g controller command

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from "./app.service";
 import { ImageController } from "./image/image.controller";
 import { ImageService } from "./image/image.service";
 import { TestResourceModule } from './test-resource/test-resource.module';
+import { TestControllerController } from './test-controller/test-controller.controller';
 
 @Module({
   imports: [TestResourceModule],
-  controllers: [AppController, ImageController],
+  controllers: [AppController, ImageController, TestControllerController],
   providers: [AppService, ImageService],
 })
 export class AppModule {}
