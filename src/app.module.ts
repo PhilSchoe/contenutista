aae89deeb457877b5cec98d82bacce64116f6b29
PhilSchoe <p.schoemacker@gmx.de>
2025-05-01 10:21:05 +0200

    Try nest g resource command

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ImageController } from "./image/image.controller";
 import { ImageService } from "./image/image.service";
+import { TestResourceModule } from './test-resource/test-resource.module';
 
 @Module({
-  imports: [],
+  imports: [TestResourceModule],
   controllers: [AppController, ImageController],
   providers: [AppService, ImageService],
 })
