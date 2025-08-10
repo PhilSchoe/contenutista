e2afe8148d4b98e18372cf37667301e357bbc308
PhilSchoe <p.schoemacker@gmx.de>
2025-08-10 21:02:41 +0200

    Move jest config to separate file

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  transformIgnorePatterns: [
+    // Jest needs to transform following packages to CommonJS, as they are ESM
+    "node_modules/(?!(nanoid)/)",
+  ],
+};
+
+export default config;
